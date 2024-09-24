a5b7abc5540ff842735a97ddc4196dd629371f66
si-lv-ia <rahmansilvia0@gmail.com>
2024-09-24 17:05:21 +0600

    1st commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function addNumber( number1,number2) {
+    const totalNumber = number1 + number2;
+    console.log ("Your Additional Number Is = " + totalNumber)
+}
+
+addNumber(300,400);
+
+function subNumber( number1,number2) {
+    const totalNumber = number1 - number2;
+    console.log ("Your sub Number Is = " + totalNumber)
+}
+
+subNumber(500,400);
+
+
+
+function multipulNumber( number1,number2) {
+    const totalNumber = number1 * number2;
+    console.log ("Your multipul Number Is = " + totalNumber)
+}
+
+multipulNumber(300,400);
+
+
+function devideNumber( number1,number2) {
+    const totalNumber = number1 / number2;
+    console.log ("Your Devidetion Number Is = " + totalNumber)
+}
+
+devideNumber(200,100);
